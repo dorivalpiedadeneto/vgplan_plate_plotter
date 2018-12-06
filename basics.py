fe1d90b48e9d1c2d6f4b623f1525d0574f9afff7
Dorival Piedade Neto <dpiedade@sc.usp.br>
2018-12-06 10:46:52 -0200

    Added main and basics

---
+++ basics.py
@@ -0,0 +1,7 @@
+
+
+__VERSION__ = (0,1,0)
+
+def version():
+    global __VERSION__
+    return __VERSION__
---
+++ main.py
@@ -0,0 +1,7 @@
+# vgplan_plate_plotter main
+from basics import *
+
+if __name__ == "__main__":
+    v, sv, ssv = version()
+    print "VGPlan Plate Plotter version %d.%d.%d"%(v,sv,ssv)
+
