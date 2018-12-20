bc70f6cab659cba868a73fb56815b197fd2a2e83
Dorival Piedade Neto <dpiedade@sc.usp.br>
2018-12-20 10:59:50 -0200

    Creation of function to read results

--- input_reader.py
+++ input_reader.py
@@ -47,13 +47,12 @@ def get_elements_data(data):
     for line in lines:
         terms = line.split()
         if len(terms) == 8:
-            try:
-                index = int(terms[0])
-                ni = int(terms[1])
-                nj = int(terms[3])
-                elements[index] = (ni, nj)
-            except:
-                print 'error!'
+            index = int(terms[0])
+            ni = int(terms[1])
+            nj = int(terms[3])
+            sec_type = int(terms[5])
+            cosine = float(terms[7])
+            elements[index] = {'nodes':(ni, nj),'sec':sec_type, 'cos':cosine}
     return elements
 
 def get_results_data(data):
@@ -65,7 +64,49 @@ def get_results_data(data):
         for part in data_parts[1:]:
             lc = part[:part.find('(GRELHA')].strip()
             results[lc] = {}
-            # Get data in data part 
+            results_ = {}
+            # Get data in data part
+            
+            # Displacements
+            start = part.find('NO               DESLOC Z')
+            end = part.find('ESFORCOS NAS EXTREMIDADES DAS BARRAS')
+            #print "Starts at %d and ends at %d"%(start, end)
+            lines = part[start:end].split('\r\n')
+            displacements = {}
+            for line in lines:
+                line_data = line.split()
+                if len(line_data) == 4:
+                    node = int(line_data[0])
+                    vdisp = float(line_data[1])
+                    displacements[node] = vdisp
+            # Internal forces
+            start = part.find('BARRA          NO          CORTANTE')
+            end = part.find('RESULTANTES NODAIS')
+            lines = part[start:end].split('\r\n')
+            print "Starts at %d and ends at %d"%(start, end)
+            lines = part[start:end].split('\r\n')
+            iforces = {}
+            for i in range(len(lines)):
+                line = lines[i]
+                line_data = line.split()
+                if len(line_data) == 5:
+                    next_line_data = lines[i+1].split()
+                    elem = int(line_data[0])
+                    ni = int(line_data[1])
+                    nj = int(next_line_data[0])
+                    Vi = float(line_data[2])
+                    Vj = float(next_line_data[1])
+                    Mi = float(line_data[3])
+                    Mj = float(next_line_data[2])
+                    Ti = float(line_data[4])
+                    Tj = float(next_line_data[3])
+                    ifs = {'nodes':(ni,nj),'V':(Vi,Vj),'M':(Mi,Mj),'T':(Ti,Tj)}
+                    iforces[elem] = ifs
+ 
+
+            results[lc]['displacements'] = displacements
+            results[lc]['internal_forces'] = iforces
+
     return results
 
         
