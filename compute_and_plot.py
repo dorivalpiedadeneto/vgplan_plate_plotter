67aae6449358de14f02c97e415082a560f70a6fb
Dorival Piedade Neto <dpiedade@sc.usp.br>
2019-01-03 10:04:12 -0200

    Advancing in computing the internal forces

--- compute_and_plot.py
+++ compute_and_plot.py
@@ -21,26 +21,82 @@ def compute_results(nodes, elements, results, section_data):
     nodal_forces = {}
     for load_case in results.keys():
         nf = {}
+        internal_forces = results[load_case]['internal_forces']
         for element, result in internal_forces.iteritems():
             element_data = elements[element]
             ni, nj = element_data['nodes']
             sec_num = element_data['section']
             cosine = element_data['cosine']
-            sine = sqrt(1.0 - cossine ** 2)
+            sine = sqrt(1.0 - cosine ** 2)
             width = section_data[sec_num]
             Mi, Mj = result['M']
             Ti, Tj = result['T']
             Vi, Vj = result['V']
             # Node i
-            mxi = - sine * M / width 
-            myi = cosine * M / width
-            Mxyi = (sine + cosine) * T / width
+            mxi = - sine * Mi / width 
+            myi = cosine * Mi / width
+            mxyi = (sine + cosine) * Ti / width
             vi = Vi / width 
 
             # Node j
-            mxi = - sine * M / width 
-            myi = cosine * M / width
-            Mxyi = (sine + cosine) * T / width
-            vi = Vi / width 
+            mxj = - sine * Mj / width 
+            myj = cosine * Mj / width
+            mxyj = (sine + cosine) * Tj / width
+            vj = Vj / width 
+            
+            # 1st node
+            if nf.has_key(ni):
+                nf_ = nf[ni]
+                nf_['mx'].append(mxi)
+                nf_['my'].append(myi)
+                nf_['mxy'].append(mxyi)
+                nf_['v'].append(vi)
+            else:
+                nf_ = {}
+                nf_['mx'] = [mxi]
+                nf_['my'] = [myi]
+                nf_['mxy'] = [mxyi]
+                nf_['v'] = [vi]
+                nf[ni] = nf_
+
+            # 2nd node
+            if nf.has_key(nj):
+                nf_ = nf[nj]
+                nf_['mx'].append(mxj)
+                nf_['my'].append(myj)
+                nf_['mxy'].append(mxyj)
+                nf_['v'].append(vj)
+            else:
+                nf_ = {}
+                nf_['mx'] = [mxj]
+                nf_['my'] = [myj]
+                nf_['mxy'] = [mxyj]
+                nf_['v'] = [vj]
+                nf[nj] = nf_
+
+        # Computing mean values
+        tol = 1.0e-6
+        nfm = {} # Nodal force mean value
+        for k,nf_ in nf.items():
+            print '-> Node #%d'%k
+            nfm_ = {}
+            for iftype in ('mx','my','mxy','v'):
+                print '  - %s: %s'%(iftype,str(nf_[iftype]))
+                count = 0
+                val = 0.0
+                for v in nf_[iftype]:
+                    if abs(v) > tol:
+                        count += 1
+                        val += v
+                if count > 0:
+                    nfm_[iftype] = val / float(count)
+                else:
+                    nfm_[iftype] = 0.0
+                print '   -> Mean value: %f'%nfm_[iftype]
+            nfm[k] = nfm_
+        nodal_forces[load_case] = {"all":nf,"mean":nfm}
+
+    return nodal_forces
+
 
 
